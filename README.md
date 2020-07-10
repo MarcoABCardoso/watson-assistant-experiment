d36039e79ba357acba175514fb04ea4f3ecd2076
Marco Aurélio Cardoso <mbigelli@br.ibm.com>
2020-07-10 16:33:17 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,58 @@
+<h1 align="center">Welcome to marcao-wa-experiment 👋</h1>
+<p>
+  <a href="https://www.npmjs.com/package/marcao-wa-experiment" target="_blank">
+    <img alt="Version" src="https://img.shields.io/npm/v/marcao-wa-experiment.svg">
+  </a>
+  <a href="#" target="_blank">
+    <img alt="License: ISC" src="https://img.shields.io/badge/License-ISC-yellow.svg" />
+  </a>
+</p>
+
+> Runs K-Fold cross validation on Watson Assistant Skill.
+
+## Install
+
+```sh
+npm install
+```
+
+## Usage
+
+```sh
+marcao-wa-experiment
+
+Marcão Experiment Script
+
+  Runs K-Fold cross validation on Watson Assistant Skill. 
+
+Options
+
+  -h, --help                  Print usage instructions.                         
+  -a, --apikey string         Watson Assistant API Key.                         
+  -w, --workspace_id string   Watson Assistant workspace ID.                    
+  -u, --url string            Watson Assistant base URL.                        
+  -n, --num_folds number      Number of folds. Default: 3                       
+  -v, --version string        Watson Assistant API version. Default: 2020-07-01 
+  -o, --output string         Output file. Default: results.json                
+
+Output
+
+  Experiment results in JSON format
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **Marco Cardoso**
+
+* Github: [@MarcoABCardoso](https://github.com/MarcoABCardoso)
+* LinkedIn: [@marco-cardoso](https://linkedin.com/in/marco-cardoso)
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
---
+++ bin/index.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+
+const commandLineArgs = require('command-line-args')
+const commandLineUsage = require('command-line-usage')
+const Assistant = require('../lib/assistant')
+const fs = require('fs')
+
+let args = [
+    { name: 'help', alias: 'h', type: Boolean, defaultValue: false, description: 'Print usage instructions.' },
+    { name: 'apikey', alias: 'a', type: String, description: 'Watson Assistant API Key.' },
+    { name: 'workspace_id', alias: 'w', type: String, description: 'Watson Assistant workspace ID.' },
+    { name: 'url', alias: 'u', type: String, description: 'Watson Assistant base URL.' },
+    { name: 'num_folds', alias: 'n', type: Number, defaultValue: 3, description: 'Number of folds. Default: 3' },
+    { name: 'version', alias: 'v', type: String, defaultValue: '2020-07-01', description: 'Watson Assistant API version. Default: 2020-07-01' },
+    { name: 'output', alias: 'o', type: String, defaultValue: 'results.json', description: 'Output file. Default: results.json' },
+]
+const sections = [
+    { header: 'Marcão Experiment Script', content: 'Runs K-Fold cross validation on Watson Assistant Skill.' },
+    { header: 'Options', optionList: args },
+    { header: 'Output', content: 'Experiment results in JSON format' },
+]
+const options = commandLineArgs(args)
+
+if (options.help || (
+    !options.apikey ||
+    !options.workspace_id ||
+    !options.url
+))
+    return console.log(commandLineUsage(sections))
+
+
+async function main() {
+    const assistant = new Assistant({ version: options.version, apikey: options.apikey, url: options.url, })
+    let results = await assistant.runExperiment(options.workspace_id, options.num_folds, true)
+    fs.writeFileSync(options.output, JSON.stringify(results, null, 4))
+}
+
+
+main()
